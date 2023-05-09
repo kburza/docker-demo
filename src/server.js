f7314d1a1e5a868322d9387a2428c85275e87803
Kacper Burza <65431499+kburza@users.noreply.github.com>
2023-05-09 10:39:50 -0400

    Removed audio player

--- src/server.js
+++ src/server.js
@@ -20,31 +20,17 @@ app.get("/", (req, res) => {
       \\\__/| \\\__/ \\\__ |\\\_ \\\__  |  
 `;
 
-  const audioTag = `
-    <audio id="audioPlayer" controls autoplay>
-      <source src="https://ucf2efe40e46047c18a4f394fed1.dl.dropboxusercontent.com/cd/0/inline/B7ocIWeoezzLk3Z6gK3VYPanaG2_JbF9t3XOCfaoDk3OueWsyHpSOEcE13pTxZkgk8wYa7Ifx5jrPwb9NhOkWMgnbkIR3IfOupxYzdRPRGXuWy6f6QuVKoUsI5jhEv_hdZOy1v5hNRIM-pvQQqXQpVDf6NabQhFoFsilb-pDcs0Hdg/file#" type="audio/mpeg">
-    </audio>
-  `;
-
   const cssStyle = `
     <style>
       body {
         background-color: black;
         color: white;
       }
-
-      audio:focus {
-        outline: none;
-      }
-
-      #audioPlayer:focus + pre {
-        color: green;
-      }
     </style>
   `;
 
   res.set("Content-Type", "text/html");
-  res.send(`${cssStyle}<pre>${asciiArt}</pre>${audioTag}`);
+  res.send(`${cssStyle}<pre>${asciiArt}</pre>`);
 });
 
 app.listen(3200, function () {
